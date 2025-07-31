bd623294beaf86c53801ec0e20bb8600aba82e17
thaque2050 <tarique.haq@gmail.com>
2025-07-31 03:42:40 -0400

    2.9 psychologist profile

--- psychologist/app/langgraph/agent_graph.py
+++ psychologist/app/langgraph/agent_graph.py
@@ -64,7 +64,7 @@
 
 
 from langgraph.graph import StateGraph, END
-# from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables import RunnableLambda
 from typing import TypedDict, List, Dict, Union
 
 class AgentState(TypedDict):
